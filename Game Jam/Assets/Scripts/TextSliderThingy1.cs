ac3df503d7546cb2960ebf62c41302d54a5cd513
Aradhan J Furtado <52329039+DNA-Batman@users.noreply.github.com>
2024-03-18 20:27:36 -0300

    Final changes

--- Game Jam/Assets/Scripts/TextSliderThingy1.cs
+++ Game Jam/Assets/Scripts/TextSliderThingy1.cs
@@ -7,13 +7,13 @@ using UnityEngine.SceneManagement;
 public class TextSliderThingy1 : MonoBehaviour
 {
     public AudioClip audioClip;
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
     public TMP_Text[] tmproObjects;
     private int currentIndex = 0;
 
     void Start()
     {
-        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+       // AudioSource audioSource = gameObject.AddComponent<AudioSource>();
         StartCoroutine(ShowSequentially());
     }
 
@@ -24,12 +24,12 @@ public class TextSliderThingy1 : MonoBehaviour
         while (currentIndex < tmproObjects.Length)
         {
             tmproObjects[currentIndex].gameObject.SetActive(true);
-            yield return new WaitForSeconds(1f); // Wait for 2 seconds before showing the next TMPro object
+            yield return new WaitForSeconds(3f); // Wait for 2 seconds before showing the next TMPro object
             tmproObjects[currentIndex].gameObject.SetActive(false);
             currentIndex++;
 
         }
-        audioSource.PlayOneShot(audioClip);
-        SceneManager.LoadScene("boss");
+        //audioSource.PlayOneShot(audioClip);
+        SceneManager.LoadScene("Boss");
     }
 }
